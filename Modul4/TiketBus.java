4cd47d656032e8f7b7fca4469b74f9a88e76e3e7
sarahsimanjuntak133-wq <sarahsimanjuntak133@gmail.com>
2026-03-29 10:57:54 +0700

    Create TiketBus.java

---
+++ Modul4/TiketBus.java
@@ -0,0 +1,29 @@
+public class TiketBus extends Tiket {
+
+    @Override
+    public void pesan() {
+        this.tujuan = "Bandung";
+    }
+
+    @Override
+    public void pesan(String n, String t) {
+        if (t.equals("Makassar")) {
+            this.tujuan = "Makassar";
+            System.out.println("Maaf " + n + ", tiket tidak dapat dipesan.");
+        }
+    }
+
+    public void pesan(String nama) {
+        this.namaPenumpang = nama;
+    }
+
+    @Override
+    public void tampil() {
+        if (!this.tujuan.equals("Makassar")) {
+            super.tampil();
+        } else {
+            System.out.println("Tiket tujuan Makassar tidak tersedia.");
+            System.out.println("--------------------");
+        }
+    }
+}
