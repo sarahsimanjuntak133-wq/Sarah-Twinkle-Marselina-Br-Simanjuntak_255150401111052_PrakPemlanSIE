d675580bffcd48aa1ec3d31ebcfaa2c28fb4f54e
sarahsimanjuntak133-wq <sarahsimanjuntak133@gmail.com>
2026-04-15 10:29:40 +0700

    Create NodeDLL.java

---
+++ Modul6/NodeDLL.java
@@ -0,0 +1,25 @@
+public class Node {
+    Object data;
+    Node next, prev;
+
+    Node() {
+    }
+
+    Node(Object data) {
+        this.data = data;
+    }
+
+    public static void main(String[] args) {
+        Node nodeA = new Node("A");
+        Node nodeB = new Node("B");
+        Node nodeC = new Node("C");
+
+        nodeA.next = nodeB;
+        nodeA.prev = nodeC;
+
+        System.out.println("node: " + nodeA);
+        System.out.println("data: " + nodeA.data);
+        System.out.println("next data: " + nodeA.next.data);
+        System.out.println("next prev: " + nodeA.prev.data);
+    }
+}
